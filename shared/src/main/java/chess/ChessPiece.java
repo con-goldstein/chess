b461f854c8ec6b3e0c451eec033affc39cf30cab
con-goldstein <connorgold04@gmail.com>
2025-06-24 23:00:45 +0100

    Started Project and added code for places a bishop can move to (bishopMoveUntilEdge test)

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -9,8 +11,11 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
-
+private ChessGame.TeamColor pieceColor;
+private ChessPiece.PieceType type;
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,16 +34,15 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
-
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
@@ -47,6 +51,91 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+    // get row and col where piece is
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+
+    // name of piece
+        ChessPiece.PieceType piece = board.getPiece(myPosition).getPieceType();
+
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        switch(piece){
+        case BISHOP:
+        //up right
+            validMoves.addAll(UpRight(myPosition, row, col));
+        //up left
+            validMoves.addAll(UpLeft(myPosition, row, col));
+        //up right
+            validMoves.addAll(BottomRight(myPosition, row, col));
+        //bottom left
+            validMoves.addAll(BottomLeft(myPosition, row, col));
+        }
+
+        return validMoves;
+    }
+
+    public Collection<ChessMove> UpRight(ChessPosition startposition, int row, int col){
+        Collection<ChessMove> moves = new ArrayList<>();
+        while ((row < 8 && col < 8) && (row > 1 && col > 1)) {
+            row += 1;
+            col += 1;
+            ChessPosition finalPosition = new ChessPosition(row, col);
+            ChessMove chessmove = new ChessMove(startposition, finalPosition, null);
+            moves.add(chessmove);
+        }
+        return moves;
+    }
+
+    public Collection<ChessMove> UpLeft(ChessPosition startposition, int row, int col){
+        Collection<ChessMove> moves = new ArrayList<>();
+        while ((row < 8 && col < 8) && (row > 1 && col > 1)) {
+            row -= 1;
+            col += 1;
+            ChessPosition finalPosition = new ChessPosition(row, col);
+            ChessMove chessmove = new ChessMove(startposition, finalPosition, null);
+            moves.add(chessmove);
+        }
+        return moves;
+    }
+    public Collection<ChessMove> BottomRight(ChessPosition startposition, int row, int col){
+        Collection<ChessMove> moves = new ArrayList<>();
+        while ((row < 8 && col < 8) && (row > 1 && col > 1)) {
+            row += 1;
+            col -= 1;
+            ChessPosition finalPosition = new ChessPosition(row, col);
+            ChessMove chessmove = new ChessMove(startposition, finalPosition, null);
+            moves.add(chessmove);
+        }
+        return moves;
+    }
+    public Collection<ChessMove> BottomLeft(ChessPosition startposition, int row, int col){
+        Collection<ChessMove> moves = new ArrayList<>();
+        while ((row < 8 && col < 8) && (row > 1 && col > 1)) {
+            row -= 1;
+            col -= 1;
+            ChessPosition finalPosition = new ChessPosition(row, col);
+            ChessMove chessmove = new ChessMove(startposition, finalPosition, null);
+            moves.add(chessmove);
+        }
+        return moves;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
+
+    public String toString(){
+        String string = String.format("Color = %s, type = %s", pieceColor, type);
+        return string;
     }
 }
