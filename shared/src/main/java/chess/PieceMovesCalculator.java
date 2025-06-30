c76cb029e4a8dd8aa96b25611826eb0d1fc41d0c
con-goldstein <connorgold04@gmail.com>
2025-06-30 13:45:15 -0600

    Created PieceMovesCalculator, along with AddToBoard, AddToMoves and FinishMoves which I took from BishopMovesCalculator to make code simpler to read and usable for the other classes

---
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -0,0 +1,38 @@
+package chess;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+
+interface PieceMovesCalculator {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
+
+default Collection<ChessMove> AddToBoard(int row, int col, ChessGame.TeamColor TeamColor, ChessPosition startposition,
+                        Collection<ChessMove> moves, ChessBoard board){
+    ChessPosition newPosition = new ChessPosition(row, col);
+    ChessPiece newPiece = board.getPiece(newPosition);
+    moves = AddToMoves(newPiece, TeamColor, row, col, startposition, moves, newPosition);
+    return moves;
+}
+
+    default Collection<ChessMove> AddToMoves(ChessPiece newPiece, ChessGame.TeamColor TeamColor, int row,
+                                             int col, ChessPosition startposition, Collection<ChessMove> moves, ChessPosition newPosition) {
+        //check if there is a piece on the square or not (null)
+        if (newPiece != null) {
+            ChessGame.TeamColor newPieceColor = newPiece.getTeamColor();
+            //if piece, check if color is same or opposite
+            if (TeamColor != newPieceColor) {
+                ChessPosition finalPosition = new ChessPosition(row, col);
+                moves = FinishMoves(startposition, finalPosition, moves);
+            }
+        } else {
+            ChessPosition finalPosition = new ChessPosition(row, col);
+            moves = FinishMoves(startposition, finalPosition, moves);
+        }
+        return moves;
+    }
+    default Collection<ChessMove> FinishMoves(ChessPosition startPosition, ChessPosition finalPosition, Collection<ChessMove> moves){
+        ChessMove chessmove = new ChessMove(startPosition, finalPosition, null);
+        moves.add(chessmove);
+        return moves;
+    }
+}
