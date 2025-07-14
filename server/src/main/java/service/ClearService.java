e4a96c826f84c9f805e9ae1bba1b482fb0390c4f
con-goldstein <connorgold04@gmail.com>
2025-07-14 17:54:02 -0600

    Created Handler with register function functioning and working (no official tests, only tested on test website)

---
+++ server/src/main/java/service/ClearService.java
@@ -0,0 +1,5 @@
+package service;
+
+public class ClearService {
+//    public ClearResult clear();
+}
---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,7 @@
+package service;
+
+public class GameService {
+//    public CreateResult create(CreateRrequest createRequest){}
+//    public JoinResult join(JoinRequest joinRequest){}
+//    public ListResult list(ListRequest listRequest){}
+}
