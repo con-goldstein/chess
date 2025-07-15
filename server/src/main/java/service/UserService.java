e68e9b4a578b9ac942ec1595aa10a738ad3d6874
con-goldstein <connorgold04@gmail.com>
2025-07-15 14:06:24 -0600

    Completed login function in UserService

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -5,6 +5,25 @@ import Results.*;
 import spark.*;
 
 public class UserService {
+    public static LoginResult login(LoginRequest LoginRequest, Response res, UserDAO user) throws BadRequestException,
+            UnauthorizedException{
+        if (LoginRequest.username() == null || LoginRequest.password() == null){
+            throw new BadRequestException("Missing username or password");
+        }
+        boolean userFound = user.findUser(LoginRequest.username());
+        if (!userFound){
+            throw new BadRequestException("User not found");
+        }
+        //user found, check if passwords match
+        boolean match = user.match(LoginRequest.username(), LoginRequest.password());
+        if (match){
+            String authToken = user.addAuthToken(LoginRequest.username());
+            return new LoginResult(LoginRequest.username(), authToken);
+        }
+        else{
+            throw new UnauthorizedException("Passwords do not match");
+        }
+    }
     public static RegisterResult register(RegisterRequest registerRequest, Response res, UserDAO user)
             throws DataAccessException, AlreadyTakenException, BadRequestException {
 
@@ -15,14 +34,14 @@ public class UserService {
         }
 
         //true is user is found
-        boolean userFound = user.findUser(registerRequest);
+        boolean userFound = user.findUser(registerRequest.username());
         //if no user is found:
         if (!userFound){
             //add user to database
              user.createUser(registerRequest, res);
 
             //add authToken and add to database
-            String authToken = user.addAuthToken(registerRequest);
+            String authToken = user.addAuthToken(registerRequest.username());
             return new RegisterResult(registerRequest.username(), authToken);
         }
         else{
