43d6e0b8d4735fc87891e64514a69e8deb6eaf7d
con-goldstein <connorgold04@gmail.com>
2025-07-15 16:17:10 -0600

    Made AuthDAO so that it is being used, MemoryAuthDAo has the authDatabase with clear, findAuthToken, createAuth() and delete()

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,11 @@
 package dataaccess;
 
+import Requests.RegisterRequest;
+import spark.Response;
+
 public interface AuthDAO {
+    String addAuthToken(String username);
+    void clear();
+    boolean findAuthToken(String authToken);
+    void delete(String authToken);
 }
--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,35 @@
 package dataaccess;
 
+import model.AuthData;
+
+import java.util.HashMap;
+import java.util.UUID;
+
 public class MemoryAuthDAO implements AuthDAO {
+    private final HashMap<String, AuthData> authDatabase = new HashMap<>();
+
+    public String addAuthToken(String username){
+        //create authToken
+        String authToken = createAuth();
+        //add user data into database
+        AuthData authData = new AuthData(authToken, username);
+        authDatabase.put(authToken, authData);
+        return authToken;
+    }
+
+
+    public String createAuth(){
+        return UUID.randomUUID().toString();
+    }
+    public void clear(){
+        authDatabase.clear();
+    }
+
+    public boolean findAuthToken(String authToken){
+        return (authDatabase.get(authToken) != null);
+    }
+
+    public void delete(String authToken){
+            authDatabase.remove(authToken);
+    }
 }
