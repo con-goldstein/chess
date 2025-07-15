f8a542fe1cf441f0e39e7d742cedd5adc5b51643
con-goldstein <connorgold04@gmail.com>
2025-07-15 16:17:54 -0600

    created clear with MemoryGameDAO

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,14 @@
 package dataaccess;
 
-public class MemoryGameDAO implements  GameDAO{
+import model.UserData;
+
+import java.util.HashMap;
+
+public class MemoryGameDAO implements GameDAO{
+    private final HashMap<String, UserData> userDatabase = new HashMap<>();
+
+    public void clear() {
+        userDatabase.clear();
+    }
+
 }
