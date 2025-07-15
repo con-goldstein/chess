83e3bacb46cf83000f69ee312291b910b4c006c8
con-goldstein <connorgold04@gmail.com>
2025-07-15 16:18:52 -0600

    changed to correct message in Unauthorized.java

--- server/src/main/java/server/exception/Unauthorized.java
+++ server/src/main/java/server/exception/Unauthorized.java
@@ -5,6 +5,6 @@ import spark.*;
 public class Unauthorized {
     public static Object response(Response res){
         res.status(401);
-        return "{ \"Message\": \"unauthorized\"}";
+        return "{ \"message\": \"Error: unauthorized\"}";
     }
 }
