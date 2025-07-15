e01c443b31cbe93981d046320ba5c86b1380eeb1
con-goldstein <connorgold04@gmail.com>
2025-07-15 17:40:53 -0600

    added list function in gameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,7 +1,17 @@
 package service;
 
+import dataaccess.*;
+import model.GameData;
+import java.util.HashSet;
+
 public class GameService {
 //    public CreateResult create(CreateRrequest createRequest){}
 //    public JoinResult join(JoinRequest joinRequest){}
-//    public ListResult list(ListRequest listRequest){}
+    public static HashSet<GameData> list(String authToken, AuthDAO authDAO, GameDAO gameDAO) throws UnauthorizedException{
+        boolean foundToken = authDAO.findAuthToken(authToken);
+        if (!foundToken){
+            throw new UnauthorizedException("could not find authToken");
+        }
+        return gameDAO.findGames();
+    }
 }
