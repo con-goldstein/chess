b5b09e43169a5105f3ac8e5c44e3e05e437d44c2
con-goldstein <connorgold04@gmail.com>
2025-07-16 14:43:08 -0600

    committing uncommited files

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,34 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+        Handler handler = new Handler();
+        // Register your endpoints and handle exceptions here.
+        Spark.post("/user", handler::register);
+        Spark.delete("/db", handler::clear);
+        Spark.post("/session", handler::login);
+        Spark.delete("/session", handler::logout);
+        Spark.get("/game", handler::listGames);
+        Spark.post("/game", handler::createGame);
+        Spark.put("/game", handler::joinGame);
+        //This line initializes the server and can be removed once you have a functioning endpoint
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+
+}
