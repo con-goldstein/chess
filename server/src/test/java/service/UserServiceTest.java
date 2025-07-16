b445f9d06acf19a09bc7b081b120c5f8c6771f14
con-goldstein <connorgold04@gmail.com>
2025-07-16 13:15:40 -0600

    Successfully has one good and bad test for register, login and logout

---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,94 @@
+package service;
+
+import Requests.LoginRequest;
+import Requests.LogoutRequest;
+import Requests.RegisterRequest;
+import Results.RegisterResult;
+import dataaccess.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import model.UserData;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.crypto.Data;
+
+class UserServiceTest {
+    private static UserDAO user;
+    private static AuthDAO auth;
+    private static GameDAO game;
+    private static RegisterRequest request;
+
+
+    @BeforeEach
+    public void beforeAll() {
+          user = new MemoryUserDAO();
+          auth = new MemoryAuthDAO();
+          game = new MemoryGameDAO();
+         request = new RegisterRequest("username", "password", "email");
+
+    }
+
+    @Test
+    //create new user
+    public void goodRegisterTest() throws DataAccessException, BadRequestException, AlreadyTakenException{
+    UserService.register(request, user, auth);
+    }
+    //Assertions.asserDoesNotThrow
+
+    @Test
+    //registering multiple people with the same username
+    public void badRegisterTest() throws DataAccessException, BadRequestException, AlreadyTakenException{
+        RegisterRequest duplicate = new RegisterRequest("username","password", "email");
+        assertThrows(AlreadyTakenException.class, () ->{
+            UserService.register(request, user, auth);
+            UserService.register(duplicate, user, auth);
+        });
+    }
+
+    @Test
+    //registers and logs in user
+    public void goodLoginTest() throws BadRequestException, UnauthorizedException, AlreadyTakenException,
+            DataAccessException{
+        LoginRequest loginRequest = new LoginRequest("username", "password");
+        UserService.register(request, user, auth);
+        UserService.login(loginRequest, user, auth);
+    }
+
+    @Test
+    //Logs in user with wrong password
+    public void badLoginTest() throws BadRequestException, UnauthorizedException, AlreadyTakenException,
+            DataAccessException{
+        assertThrows(UnauthorizedException.class, () ->{
+            UserService.register(request, user, auth);
+            UserService.login(new LoginRequest("username", "wrongPassword"), user, auth);
+        });
+
+    }
+
+    @Test
+    //successfully logs out user
+    public void goodLogoutTest()throws BadRequestException, UnauthorizedException, AlreadyTakenException,
+            DataAccessException{
+        LoginRequest loginRequest = new LoginRequest("username", "password");
+        RegisterResult result = UserService.register(request, user, auth);
+        UserService.login(loginRequest, user, auth);
+        String authToken = result.authToken();
+        LogoutRequest logoutRequest = new LogoutRequest(authToken);
+        UserService.logout(logoutRequest, auth);
+    }
+
+    @Test
+    //wrong authToken, invalid logout test
+    public void badLogoutTest() {
+        assertThrows(UnauthorizedException.class, () -> {
+            LoginRequest loginRequest = new LoginRequest("username", "password");
+            RegisterResult result = UserService.register(request, user, auth);
+            UserService.login(loginRequest, user, auth);
+            String authToken = result.authToken();
+            LogoutRequest logoutRequest = new LogoutRequest("wrong");
+            UserService.logout(logoutRequest, auth);
+        });
+    }
+}
