7b84c00bad507594e3c4b42d13e705f81295df0f
con-goldstein <connorgold04@gmail.com>
2025-07-15 20:53:50 -0600

    Created createGameData function in MemoryGameDAO, created random gameID and creates new game

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,9 +1,9 @@
 package dataaccess;
 
-import model.GameData;
-import model.UserData;
-
-import java.util.HashMap;
+import Results.CreateResult;
+import chess.ChessGame;
+import model.*;
+import java.util.Random;
 import java.util.HashSet;
 
 public class MemoryGameDAO implements GameDAO{
@@ -14,5 +14,17 @@ public class MemoryGameDAO implements GameDAO{
     }
     public HashSet<GameData> findGames(){
         return gameDatabase;
+    }
+
+    public CreateResult createGameData(String gameName, String authToken) throws BadRequestException{
+        Random random = new Random();
+        int gameID = random.nextInt(1000);
+        try {
+            GameData gameData = new GameData(gameID, null, null, gameName, new ChessGame());
+            return new CreateResult(gameData.GameID());
+        }
+        catch(Exception e){
+            throw new BadRequestException("could not");
+        }
     }
 }
