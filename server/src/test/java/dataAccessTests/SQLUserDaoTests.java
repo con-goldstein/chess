4434917dbf4947509539fc609c0ed22463dbf3e8
con-goldstein <connorgold04@gmail.com>
2025-07-21 16:44:46 -0600

    Test cases for createUser, findUser, and match functions

--- server/src/test/java/dataAccessTests/SQLUserDaoTests.java
+++ server/src/test/java/dataAccessTests/SQLUserDaoTests.java
@@ -10,19 +10,21 @@ import requests.*;
 import java.sql.SQLException;
 
 public class SQLUserDaoTests {
-    private static UserDAO userDAO;
-    private static RegisterRequest request;
+    private UserDAO userDAO;
+    private RegisterRequest request;
 
     @BeforeEach
-    public void beforeALL() throws SQLException, DataAccessException {
+    public void beforeEach() throws SQLException, DataAccessException {
         userDAO = new SQLUserDAO();
+        userDAO.clear();
         request = new RegisterRequest("username", "password", "email");
 
     }
+
     @Test
-    public void goodCreateuserTest() throws DataAccessException{
-        userDAO.createUser(new RegisterRequest("username", "password", "@gmail.com"));
-        try (var conn = DatabaseManager.getConnection()){
+    public void goodCreateuserTest() throws DataAccessException {
+        userDAO.createUser(request);
+        try (var conn = DatabaseManager.getConnection()) {
             var statement = "SELECT * FROM user WHERE username=?";
             var prepstatement = conn.prepareStatement(statement);
             prepstatement.setString(1, "username");
@@ -33,11 +35,46 @@ public class SQLUserDaoTests {
                 String email = result.getString("email");
                 assertEquals("username", username);
                 assertEquals("password", pass);
-                assertEquals("@gmail.com", email);
+                assertEquals("email", email);
             }
-        }
-        catch (SQLException | DataAccessException e){
+        } catch (SQLException | DataAccessException e) {
             throw new DataAccessException("could not create user");
         }
+    }
+
+    @Test
+    public void badCreateUserTest() throws DataAccessException {
+        assertThrows(DataAccessException.class, () -> {
+            userDAO.createUser(new RegisterRequest("username", "password", "email"));
+            userDAO.createUser(new RegisterRequest("username", "password", "email"));
+        });
+    }
+
+    @Test
+    public void goodFindUserTest() throws DataAccessException {
+        userDAO.createUser(request);
+        boolean found = userDAO.findUser("username");
+        assertTrue(found);
+    }
+
+    @Test
+    public void badFindUserTest() throws DataAccessException {
+        userDAO.createUser(request);
+        boolean found = userDAO.findUser("wrongUsername");
+        assertFalse(found);
+    }
+
+    @Test
+    public void goodMatchTest() throws DataAccessException {
+        userDAO.createUser(request);
+        boolean matched = userDAO.match(request.username(), request.password());
+        assertTrue(matched);
+    }
+
+    @Test
+    public void badMatchTest() throws DataAccessException {
+        userDAO.createUser(request);
+        boolean matched = userDAO.match(request.username(), "badPassword");
+        assertFalse(matched);
     }
 }
