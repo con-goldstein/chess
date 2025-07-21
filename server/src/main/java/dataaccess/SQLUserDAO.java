633bdbad2cba2d3b14697bff47bc0979f81031ef
con-goldstein <connorgold04@gmail.com>
2025-07-21 16:45:56 -0600

    SQLUserDAO with functioning createUser function, findUser function and match function

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,9 +1,6 @@
 package dataaccess;
 import model.*;
 import requests.RegisterRequest;
-
-import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 import java.sql.SQLException;
 
 public class SQLUserDAO implements UserDAO{
@@ -19,18 +16,64 @@ public class SQLUserDAO implements UserDAO{
         }
     }
     public void createUser(RegisterRequest registerRequest) throws DataAccessException{
-        try (var conn = DatabaseManager.getConnection()){
-            var statement = "INSERT INTO user (username, password, email) VALUES(?, ?, ?)";
-            var prepstatement = conn.prepareStatement(statement);
-            prepstatement.setString(1, registerRequest.username());
-            prepstatement.setString(2, registerRequest.password());
-            prepstatement.setString(3, registerRequest.email());
-            prepstatement.executeUpdate();
+        try (var conn = DatabaseManager.getConnection()) {
+            boolean userFound = findUser(registerRequest.username());
+            if (!userFound) {
+                var statement = "INSERT INTO user (username, password, email) VALUES(?, ?, ?)";
+                var preparedStatement = conn.prepareStatement(statement);
+                preparedStatement.setString(1, registerRequest.username());
+                preparedStatement.setString(2, registerRequest.password());
+                preparedStatement.setString(3, registerRequest.email());
+                preparedStatement.executeUpdate();
+            }
+            else {
+                throw new DataAccessException("error creating user");
+            }
         }
-        catch (SQLException | DataAccessException e){
+        catch (SQLException e){
             throw new DataAccessException("could not create user");
         }
     }
 
-public class MySQLUserDAO {
+    public boolean findUser(String username) {
+        try (var conn = DatabaseManager.getConnection()){
+            var statement = "SELECT * FROM user WHERE username=?";
+            var preparedStatement = conn.prepareStatement(statement);
+            preparedStatement.setString(1, username);
+            try (var rs = preparedStatement.executeQuery()) {
+                return rs.next();
+            }
+        }
+        catch (SQLException | DataAccessException e){
+            return false;
+        }
+    }
+    public boolean match(String username, String password){
+        try (var conn = DatabaseManager.getConnection()){
+            var statement = "SELECT password FROM user WHERE username=?";
+            var prepstatement = conn.prepareStatement(statement);
+            prepstatement.setString(1, username);
+            try (var rs = prepstatement.executeQuery()){
+                if (rs.next()){
+                    var tablePassword = rs.getString("password");
+                    if (tablePassword.equals(password)){
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        catch (SQLException | DataAccessException e){
+            return false;
+        }
+    }
+    public void clear(){
+        try (var conn = DatabaseManager.getConnection()){
+            var statement = "TRUNCATE user";
+            var prepstatement = conn.prepareStatement(statement);
+            prepstatement.executeUpdate();
+        }
+        catch (SQLException | DataAccessException e){}
+    }
 }
+
