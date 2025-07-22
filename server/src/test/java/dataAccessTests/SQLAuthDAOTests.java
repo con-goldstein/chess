7072a694f44f944c4f64be60e7877cf3f4622a1a
con-goldstein <connorgold04@gmail.com>
2025-07-22 12:35:59 -0600

    Finished file of AuthDao tests

--- server/src/test/java/dataAccessTests/SQLAuthDAOTests.java
+++ server/src/test/java/dataAccessTests/SQLAuthDAOTests.java
@@ -1,5 +1,6 @@
 package dataAccessTests;
 import dataaccess.*;
+import model.AuthData;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,6 +8,8 @@ import org.opentest4j.AssertionFailedError;
 import requests.*;
 
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.UUID;
 
 public class SQLAuthDAOTests {
     private AuthDAO authDAO;
@@ -23,7 +26,8 @@ public class SQLAuthDAOTests {
     @Test
     public void goodAddAuthTokenTest() throws DataAccessException, SQLException{
         String username = "username";
-        String authToken = authDAO.addAuthToken(username);
+        String authToken = UUID.randomUUID().toString();
+        authDAO.addAuthToken(username, authToken);
         try (var conn = DatabaseManager.getConnection()){
             var statement = "SELECT * FROM auth WHERE username=?";
             var preparedStatement = conn.prepareStatement(statement);
@@ -44,8 +48,9 @@ public class SQLAuthDAOTests {
 
     @Test public void badAddAuthTokenTest() {
         assertThrows(AssertionFailedError.class, () -> {
-            authDAO.addAuthToken("username");
-            authDAO.addAuthToken("username");
+            String authToken = UUID.randomUUID().toString();
+            authDAO.addAuthToken("username", authToken);
+            authDAO.addAuthToken("username", authToken);
             assertThrows(SQLException.class, () -> {try (var conn = DatabaseManager.getConnection()){
                 var statement = "SELECT * FROM auth WHERE username=?";
                 var preparedStatement = conn.prepareStatement(statement);
@@ -56,5 +61,80 @@ public class SQLAuthDAOTests {
                 }
             }});
         });
+    }
+
+    @Test public void goodfindAuthTokenTests(){
+        String authToken = UUID.randomUUID().toString();
+        authDAO.addAuthToken("Connor", authToken);
+        try (var conn = DatabaseManager.getConnection()){
+            var statement = "SELECT authToken FROM auth WHERE username=?";
+            var preparedStatement = conn.prepareStatement(statement);
+            preparedStatement.setString(1, "Connor");
+            try (var res = preparedStatement.executeQuery()){
+                if (res.next()){
+                    String dbToken = res.getString("authToken");
+                    assertEquals(authToken, dbToken);
+                }
+                else{fail("no result returned");}
+            }
+        }
+        catch (SQLException | DataAccessException e){}
+    }
+
+    @Test public void badfindAuthTokenTests(){
+        boolean foundToken = authDAO.findAuthToken("badToken");
+        assertFalse(foundToken);
+    }
+
+
+    @Test public void goodDeleteTest(){
+        try (var conn = DatabaseManager.getConnection()){
+            var statement = "SELECT authToken FROM auth WHERE username=?";
+            var preparedStatement = conn.prepareStatement(statement);
+            preparedStatement.setString(1, "username");
+            try (var res = preparedStatement.executeQuery()){
+                String authToken = res.getString("authToken");
+                authDAO.delete(authToken);
+                assertFalse(res.next());
+            }
+        }
+        catch(SQLException | DataAccessException e){}
+    }
+
+
+    @Test public void badDeleteTest(){
+        assertDoesNotThrow(() -> authDAO.delete("badauthToken"));
+    }
+
+    @Test public void goodGetAuthData(){
+        String authToken = UUID.randomUUID().toString();
+        authDAO.addAuthToken("Connor", authToken);
+        AuthData data = authDAO.getauthData(authToken);
+        try (var conn = DatabaseManager.getConnection()){
+            var statement = "SELECT * FROM auth WHERE authToken=?";
+            var preparedStatement = conn.prepareStatement(statement);
+            preparedStatement.setString(1, authToken);
+            try (var res = preparedStatement.executeQuery()){
+                if (res.next()){
+                    String dbToken = res.getString("authToken");
+                    String dbUsername = res.getString("username");
+                    assertEquals(data.authToken(), dbToken);
+                    assertEquals(data.username(), dbUsername);
+                 }
+                else{
+                    fail("could not find correct AuthData");
+                }
+            }
+        }
+        catch (SQLException | DataAccessException e){
+            fail("test failed");
+        }
+    }
+
+    @Test public void badGetAuthData(){
+        String authToken = UUID.randomUUID().toString();
+        authDAO.addAuthToken("Connor", authToken);
+        AuthData data = authDAO.getauthData("badToken");
+        assertNull(data);
     }
 }
