91eea39987c9b50060e931c67dfc2c2f6cffbd91
con-goldstein <connorgold04@gmail.com>
2025-07-29 00:48:08 -0600

    RegisterTests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,19 +1,47 @@
 package client;
 
+import dataaccess.*;
+import model.GameData;
 import org.junit.jupiter.api.*;
+import requests.*;
+import results.*;
 import server.Server;
+import Server.ServerFacade;
+
+import java.sql.SQLException;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
+    AuthDAO authDAO;
+    UserDAO userDAO;
+    GameDAO gameDAO;
+
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:8080");
     }
+
+   @BeforeEach
+   public void beforeEach() throws SQLException, DataAccessException {
+        authDAO = new SQLAuthDAO();
+        userDAO = new SQLUserDAO();
+        gameDAO = new SQLGameDAO();
+        authDAO.clear();
+        userDAO.clear();
+        gameDAO.clear();
+   }
+
+
 
     @AfterAll
     static void stopServer() {
@@ -22,8 +50,98 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void goodRegisterTest() throws UnauthorizedException, BadRequestException, AlreadyTakenException {
+        RegisterResult result = facade.register(new RegisterRequest("username", "password", "email"));
+        assertEquals("username", result.username());
+        System.out.println(result.authToken());
+        assertTrue(result.authToken().length() > 10);
     }
 
+    @Test public void badRegisterTest() throws UnauthorizedException, BadRequestException, AlreadyTakenException{
+        facade.register(new RegisterRequest("username", "password", "email"));
+        assertThrows(AlreadyTakenException.class, () -> {
+            facade.register(new RegisterRequest("username", "password", "email"));
+        });
+    }
+
+    @Test public void goodLoginTest() throws UnauthorizedException, BadRequestException, AlreadyTakenException {
+        facade.register(new RegisterRequest("username","password", "email"));
+        LoginResult result = facade.login(new LoginRequest("username", "password"));
+        assertEquals("username", result.username());
+        assertTrue(result.authToken().length() > 10);
+    }
+
+    @Test public void badLoginTest() {
+        assertThrows(UnauthorizedException.class, () -> {
+            facade.login(new LoginRequest("username", "password"));
+        });
+    }
+
+    @Test public void goodCreateTest() throws UnauthorizedException, BadRequestException, AlreadyTakenException, DataAccessException {
+        facade.register(new RegisterRequest("username","password", "email"));
+        facade.login(new LoginRequest("username", "password"));
+        facade.create("gameName");
+        HashSet<GameData> games = gameDAO.findGames();
+        assertEquals(1, games.size());
+    }
+
+    @Test public void goodCreateTest2() throws UnauthorizedException, BadRequestException, AlreadyTakenException, DataAccessException {
+        facade.register(new RegisterRequest("username","password", "email"));
+        facade.login(new LoginRequest("username", "password"));
+        facade.create("gameName");
+        facade.create("anotherGameName");
+        HashSet<GameData> games = gameDAO.findGames();
+        assertEquals(2, games.size());
+    }
+
+    @Test public void badCreateTest() throws UnauthorizedException, BadRequestException, AlreadyTakenException, DataAccessException {
+        facade.register(new RegisterRequest("username","password", "email"));
+        facade.login(new LoginRequest("username", "password"));
+        facade.create("gameName");
+        assertThrows(BadRequestException.class, () -> {
+            facade.create("gameName");
+        });
+    }
+
+    @Test public void goodListTest() throws UnauthorizedException, BadRequestException, AlreadyTakenException, DataAccessException {
+        facade.register(new RegisterRequest("username","password", "email"));
+        facade.login(new LoginRequest("username", "password"));
+        facade.create("gameName");
+        ListResult result = facade.list();
+        HashSet<GameData> games = result.games();
+        assertEquals(1, games.size());
+    }
+
+    @Test public void goodListTest2() throws UnauthorizedException, BadRequestException, AlreadyTakenException {
+        facade.register(new RegisterRequest("username","password", "email"));
+        facade.login(new LoginRequest("username", "password"));
+        facade.create("gameName");
+        facade.create("anotherGameName");
+        ListResult result = facade.list();
+        HashSet<GameData> games = result.games();
+        assertEquals(2, games.size());
+    }
+
+    @Test public void badListTest() throws UnauthorizedException, BadRequestException, AlreadyTakenException {
+        assertThrows(UnauthorizedException.class, () -> {
+            facade.list();
+        });
+    }
+
+    @Test public void goodLogoutTest() throws UnauthorizedException, BadRequestException, AlreadyTakenException, DataAccessException {
+        facade.register(new RegisterRequest("username","password", "email"));
+        facade.login(new LoginRequest("username", "password"));
+        assertTrue(facade.logout());
+        }
+
+
+        @Test public void badLogoutTest() throws UnauthorizedException, BadRequestException, AlreadyTakenException {
+        assertThrows(UnauthorizedException.class, () -> {
+            facade.logout();
+        });
+        }
+
+        @Test public void goodJoinTest(){
+
+        }
 }
