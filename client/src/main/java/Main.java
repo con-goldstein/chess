1ad909083172535ad2e11cfdc0e4ebd8a471dd5d
con-goldstein <connorgold04@gmail.com>
2025-08-08 01:31:49 -0600

    Main does not throw exception anymore

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // connect with repl in here
 //        Server server = new Server();
         Repl repl = new Repl();
